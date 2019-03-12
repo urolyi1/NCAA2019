1b51723ddf463e075a5d10ceee1d620392c0db55
urolyi1 <urolyi1@gmail.com>
2019-03-12 11:47:40 -0400

    Add simple linear reg analysis

--- R_analysis/preliminary_analysis.R
+++ R_analysis/preliminary_analysis.R
@@ -1,9 +1,9 @@
 library(readr)
+ncaa2018 <- read_csv("~/repositories/NCAA2019/statsandgames/ncaa2018gamesdata.csv")
 
-ncaa2012 <- read_csv("~/repositories/NCAA2019/statsandgames/ncaa2012gamesdata.csv")
-score1 = ncaa2012$Points1
-ncaa2012$SOS
-score2 = ncaa2012$Points2
-
-y <- lm((Points1 - Points2) ~ AdjEM1 + AdjEM2, data=ncaa2012)
-summary(y)
+#model <- lm((ncaa2018$Points1 - ncaa2018$Points2) ~ (ncaa2018$`3P%1`* (ncaa2018$`3PA1`/ ncaa2018$G1)) + 
+ #             (ncaa2018$`3P%2`* (ncaa2018$`3PA2`/ ncaa2018$G2)) + (ncaa2018$`FG%2`* (ncaa2018$`FGA2`/ ncaa2018$G2)) +
+  #            (ncaa2018$`FG%2`* (ncaa2018$`FGA2`/ ncaa2018$G2)))
+model <- lm((ncaa2018$Points1 - ncaa2018$Points2) ~ ncaa2018$AdjD1 + ncaa2018$AdjD2 + ncaa2018$AdjO1*ncaa2018$AdjT1
+            + ncaa2018$AdjO2*ncaa2018$AdjT2 + ncaa2018$`FT%1`  + ncaa2018$`FT%2`)
+summary(model)
