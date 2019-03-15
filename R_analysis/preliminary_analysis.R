9f116979daf7d80a3c0182948d26565df419751a
urolyi1 <urolyi1@gmail.com>
2019-03-15 13:22:17 -0400

    Add Linear Regression to Result Prediction Model

--- R_analysis/preliminary_analysis.R
+++ R_analysis/preliminary_analysis.R
@@ -14,12 +14,12 @@ for (i in 2:length(files)) {
   ncaaData <- rbind(ncaaData, temp) #append next year
 }
 
-startYear <- 2011
-endYear <- 2014
-testYear <- 2015
+startYear <- 2014
+endYear <- 2016
+testYear <- 2017
 
 startIndex <- (startYear - 2002) * 63 + 1
-endIndex <- (endYear - 2002) * 63 + 1
+endIndex <- ((endYear + 1) - 2002) * 63 + 1
 trainingData <- ncaaData[startIndex:endIndex,]
 
 testStart <- (testYear - 2002) * 63 + 1
@@ -27,11 +27,32 @@ testEnd <- testStart + 62
 testData <- ncaaData[testStart:testEnd,]
 
 model <- glm(Result ~ AdjO1 + AdjO2 + AdjD1 + AdjD2 + FT.1 + FT.2, data=trainingData, family="binomial")
+
+#create linear model to predict margin
+points <- lm(Points1 - Points2 ~ AdjO1 + AdjO2 + AdjD1 + AdjD2 + FT.1 + FT.2, data=trainingData)
+#predict margins for training data
+pred_margin <- predict(points, trainingData, type="response")
+model2 <- glm(Result ~ pred_margin, data=trainingData, family = "binomial")
+
+#test data predicted margin
+pred_margin2 <- predict(points, testData, type="response")
+margin <- data.frame(pred_margin = pred_margin2)
+
+#predicted outcome of games
 pred <- predict(model, testData, type="response")
+pred2 <- predict(model2, margin, type="response")
+
+#capping
 ind <- pred < .05
 pred[ind] <- .05
 ind <- pred > .95
 pred[ind] <- .95
 
+ind <- pred2 < .05
+pred2[ind] <- .05
+ind <- pred2 > .95
+pred2[ind] <- .95
+
 
 logLoss(testData$Result, pred)
+logLoss(testData$Result, pred2)
